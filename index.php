927fc7794d8e4232a1a4a7a62976c13f5917ba2e
shrlyhe <she@wesleyan.edu>
2016-08-25 16:31:50 -0400

    make table...query not fully functional

--- index.php
+++ index.php
@@ -1,1 +1,34 @@
 <?php header('Location:/index.html'); ?>
+
+
+<?php
+    $db = mysqli_connect('localhost','root','may121996','summies');
+?>
+<html>
+    <body>
+          <?php
+                $query = "SELECT * FROM `summiesOrders`;";
+                $result = mysqli_query($db, $query);
+                while($row = mysqli_fetch_assoc($result)) {
+                      // Display your datas on the page
+                }
+          ?>
+    </body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
